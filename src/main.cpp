c4b06c46dd0b660c01263677bd0a56f9fc7e8d16
ant1ler <ilyaant2004@gmail.com>
2025-07-19 23:27:10 +0300

    Add validate

--- src/main.cpp
+++ src/main.cpp
@@ -4,87 +4,68 @@
 
 void validate(FbootModel fboot, SysModel sys){
 
-    std::vector<FunctionalBlock> fb_fboot = fboot.get_fb();
-    std::vector<FunctionalBlock> fb_sys = sys.get_fb();
+    std::vector<FunctionalBlock> fb_fboot;
+    std::vector<FunctionalBlock> fb_sys;
     bool fb_validate = false;
 
-    for(int i = 0; i < fb_fboot.size(); i++){
-        for(int j = 0; j < fb_sys.size(); j++){
-            if(fb_fboot[i].get_name() == fb_sys[j].get_name()){
-                if(fb_fboot[i].get_type() == fb_sys[j].get_type()){
-                    if(fb_fboot[i].get_params() == fb_sys[j].get_params()){
-                        if(fb_fboot[i].get_resource() == fb_sys[j].get_resource().substr(fb_sys[j].get_resource().find(".") + 1, std::string::npos)){
+    for(int i = 0; i < fboot.get_fb().size(); i++){
+        for(int j = 0; j < sys.get_fb().size(); j++){
+            if(fboot.get_fb()[i].get_name() == sys.get_fb()[j].get_name()){
+                if(fboot.get_fb()[i].get_type() == sys.get_fb()[j].get_type()){
+                    if(fboot.get_fb()[i].get_params() == sys.get_fb()[j].get_params()){
+                        if(fboot.get_fb()[i].get_resource() == sys.get_fb()[j].get_resource().substr(sys.get_fb()[j].get_resource().find(".") + 1, std::string::npos)){
 
-                            std::cout << "Fboot" << std::endl << fb_fboot[i].get_name() << " " << fb_fboot[i].get_type() << std::endl;
-                            auto iter_fboot = fb_fboot.begin() + i;
-                            fb_fboot.erase(iter_fboot);
-
-                            std::cout << "Sys" << std::endl << fb_sys[j].get_name() << " " << fb_sys[j].get_type() << std::endl;
-                            auto iter_sys = fb_sys.begin() + j;
-                            fb_sys.erase(iter_sys);
-
+                            fb_fboot.push_back(fboot.get_fb()[i]);
+                            fb_sys.push_back(sys.get_fb()[j]);
                         }
                     }
                 }
             }
         }
     }
-    if(fb_fboot.size() == 0 and fb_sys.size() == 0) {fb_validate = true;}
+    if(fb_fboot.size() == fboot.get_fb().size() and fb_sys.size() == sys.get_fb().size()) {fb_validate = true;}
 
 
-    std::vector<Resource> resource_fboot = fboot.get_resource();
-    std::vector<Resource> resource_sys = sys.get_resource();
+    std::vector<Resource> resource_fboot;
+    std::vector<Resource> resource_sys;
     bool resource_validate = false;
 
-    for(int i = 0; i < resource_fboot.size(); i++){
-        for(int j = 0; j < resource_sys.size(); j++){
-            if(resource_fboot[i].get_name() == resource_sys[j].get_name().substr(resource_sys[j].get_name().find(".") + 1, std::string::npos)){
-                if(resource_fboot[i].get_type() == resource_sys[j].get_type()){
+    for(int i = 0; i < fboot.get_resource().size(); i++){
+        for(int j = 0; j < sys.get_resource().size(); j++){
+            if(fboot.get_resource()[i].get_name() == sys.get_resource()[j].get_name().substr(sys.get_resource()[j].get_name().find(".") + 1, std::string::npos)){
+                if(fboot.get_resource()[i].get_type() == sys.get_resource()[j].get_type()){
 
-                    std::cout << "Fboot" << std::endl << resource_fboot[i].get_name() << " " << resource_fboot[i].get_type() << std::endl;
-                    auto iter_fboot = resource_fboot.begin() + i;
-                    resource_fboot.erase(iter_fboot);
+                    resource_fboot.push_back(fboot.get_resource()[i]);
 
-                    std::cout << "Sys" << std::endl << resource_sys[j].get_name() << " " << resource_sys[j].get_type() << std::endl;
-                    auto iter_sys = resource_sys.begin() + j;
-                    resource_sys.erase(iter_sys);
-
+                    resource_sys.push_back(sys.get_resource()[j]);
                 }
             }
         }
     }
-    if(resource_fboot.size() == 0 and resource_sys.size() == 0) {resource_validate = true;}
+    if(resource_fboot.size() == fboot.get_resource().size() and resource_sys.size() == sys.get_resource().size()) {resource_validate = true;}
 
-    std::vector<Connection> connection_fboot = fboot.get_connection();
-    std::vector<Connection> connection_sys = sys.get_connection();
+    std::vector<Connection> connection_fboot;
+    std::vector<Connection> connection_sys;
     bool connection_validate = false;
     std::cout << "Connection" << std::endl;
-    for(int i = 0; i < connection_fboot.size(); i++){
-        for(int j = 0; j < connection_sys.size(); j++){
-            if(connection_fboot[i].get_start() == connection_sys[j].get_start()){
-                if(connection_fboot[i].get_end() == connection_sys[j].get_end()){
+    for(int i = 0; i < fboot.get_connection().size(); i++){
+        for(int j = 0; j < sys.get_connection().size(); j++){
+            if(fboot.get_connection()[i].get_start() == sys.get_connection()[j].get_start()){
+                if(fboot.get_connection()[i].get_end() == sys.get_connection()[j].get_end()){
 
-                    std::cout << "Fboot" << std::endl << connection_fboot[i].get_start() << " " << connection_fboot[i].get_end() << std::endl;
-                    auto iter_fboot = connection_fboot.begin() + i;
-                    connection_fboot.erase(iter_fboot);
+                    connection_fboot.push_back(fboot.get_connection()[j]);
 
-                    std::cout << "Sys" << std::endl << connection_sys[j].get_start() << " " << connection_sys[j].get_end() << std::endl;
-                    auto iter_sys = connection_sys.begin() + j;
-                    connection_sys.erase(iter_sys);
-                    break;
-
+                    connection_sys.push_back(sys.get_connection()[j]);
                 }
             }
         }
     }
-    if(connection_fboot.size() == 0 and connection_sys.size() == 0) {connection_validate = true;}
+    if(connection_fboot.size() == fboot.get_connection().size() and connection_sys.size() == sys.get_connection().size()) {connection_validate = true;}
 
     if(fb_validate and resource_validate and connection_validate){
         std::cout << "Fboot-файл совпадает с sys-файлом" << std::endl;
     }
     else{
-        std::cout << "Fboot" << std::endl << fb_fboot.size() << resource_fboot.size() << connection_fboot.size() << std::endl;
-        std::cout << "Sys" << std::endl << fb_sys.size() << resource_sys.size() << connection_sys.size() << std::endl;
         std::cout << "Fboot-файл не совпадает с sys-файлом" << std::endl;
     } 
 }
@@ -94,23 +75,23 @@ int main(){
     FbootModel fboot;
 
     pugi::xml_document doc;
-    if (!doc.load_file("/home/ant1ler/check_fbt_fboot_4diac/files/mai_test1.sys")) {
+    if (!doc.load_file("/home/ant1ler/check_fbt_fboot_4diac/files/parse_test.sys")) {
         std::cerr << "Ошибка загрузки XML!" << std::endl;
         return 0;
     }
 
-    std::string sys_filename = "/home/ant1ler/check_fbt_fboot_4diac/files/mai_test1.sys";
+    std::string sys_filename = "/home/ant1ler/check_fbt_fboot_4diac/files/parse_test.sys";
     sys.set_filename(sys_filename);
     sys.parse(sys.get_filename());
-    //sys.correction();
+    sys.correction();
     sys.print_structure();
 
     std::cout << std::endl;
 
-    std::string fboot_filename = "/home/ant1ler/check_fbt_fboot_4diac/files/mai_test1_FORTE_PC.fboot";
+    std::string fboot_filename = "/home/ant1ler/check_fbt_fboot_4diac/files/parse_test_FORTE_PC.fboot";
     fboot.set_filename(fboot_filename);
     fboot.parse(fboot.get_filename());
-    //fboot.correction();
+    fboot.correction();
     fboot.print_structure();
 
     validate(fboot, sys);
